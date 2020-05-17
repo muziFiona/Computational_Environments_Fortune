bfd6942ef061b62235cd3bdca4ef63050043cb15
Fiona Lei <56001183+muziFiona@users.noreply.github.com>
2020-05-17 23:57:33 +0100

    Add files via upload

---
+++ docs/fortune/fortune.js
@@ -0,0 +1,23 @@
+let angle = 0;
+let x= 0;
+
+let bg = 50;
+//let size;
+
+
+function setup() {
+ createCanvas(windowWidth, windowHeight, WEBGL);
+ //frameRate(30);
+}
+
+
+function draw() {
+  //ambientLight(255);
+  background(bg);
+  
+  mousePt();
+  //ambientMaterial(0,0,255);
+  boxT();
+  
+  
+}
