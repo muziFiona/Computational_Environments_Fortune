05b9cc1b8142aea2f772edbe1c92c7bb372ab156
Fiona Lei <56001183_muziFiona@users.noreply.github.com>
2020-05-18 01:44:47 +0100

    Create RUNES.js

---
+++ docs/fortune/fortune_js/RUNES.js
@@ -0,0 +1,12 @@
+//this will be the script for the Runes structure, drawing, meaning, the results
+
+let runes = [];
+
+function RUNE(){
+  for (let i = 0; i < 78; i+=3){
+    
+    //stroke(runes[i]* 200);
+    line(i, 0, i, 100);
+  }
+  
+}
