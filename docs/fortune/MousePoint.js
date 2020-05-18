a15bee5bd2973e29e3e447073f87f6827a758105
Fiona Lei <56001183+muziFiona@users.noreply.github.com>
2020-05-18 02:26:18 +0100

    Update MousePoint.js

--- docs/fortune/MousePoint.js
+++ docs/fortune/MousePoint.js
@@ -1,18 +1,23 @@
+// so far this is the sphere that goes with mouse and controling directional light
+
 function mousePt(){
   let dx = mouseX - width/2;
   let dy = mouseY - height/2;
+  let v = createVector(dx, dy, 1);
+  v.normalize();
   
-  directionalLight(255, 255, 0, dx, dy, 0);
+  directionalLight(255, 255, 0, v);
   //pointLight(8, 251, 0, width/2, -height/2, 100);
   
   push();
   translate(mouseX-width/2, mouseY-height/2);
-  stroke(0);
-  strokeWeight(4);
+  noStroke();
+  //stroke(0);
+  //strokeWeight(4);
   //fill(255,255,255,111);
   specularMaterial(255);
   rotateX(angle);
-  sphere(30,30,5);
+  sphere(20,20,5);
   pop();
   
   angle += 0.03;
