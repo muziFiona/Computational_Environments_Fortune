1333bfc867f3edd9231b12549f9a4e276b3a5de0
Fiona Lei <56001183+muziFiona@users.noreply.github.com>
2020-05-30 18:56:41 +0100

    Update fortune.js

--- docs/fortune/fortune.js
+++ docs/fortune/fortune.js
@@ -1,5 +1,6 @@
 let angle = 0;
-let x= 0;
+var x = [];
+var z = [];
 
 let bg = 50;
 //let size;
@@ -8,16 +9,31 @@ let bg = 50;
 function setup() {
  createCanvas(windowWidth, windowHeight, WEBGL);
  //frameRate(30);
+  for (var i=0; i<25; i++){
+    x[i] = 240 * cos(24*radians(i));
+    z[i] =  240 * sin(24*radians(i));
+  }
 }
 
 
 function draw() {
   //ambientLight(255);
   background(bg);
-  mousePt();
+  //mousePt();
   //ambientMaterial(0,0,255);
+  dotPt();
+  push();
+  rotateY(angle);
   boxT();
+  pop();
   
+  push();
+  noStroke();
+  ambientMaterial(200,111,255,255);
+  translate(0,200,100);
+  rotateX(PI/2);
+  plane(width,height);
+  pop();
   
   //RUNE();
   
