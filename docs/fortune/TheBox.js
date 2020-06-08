2bd38df8b71a1b077686cee25301ea77bca768ef
Fiona Lei <56001183+muziFiona@users.noreply.github.com>
2020-06-08 02:08:53 +0100

    Update TheBox.js

--- docs/fortune/TheBox.js
+++ docs/fortune/TheBox.js
@@ -1,4 +1,11 @@
+
 let size = 30;
+var runeStone = boxT();
+var fade ;
+var fadeAmount = 1;
+var op;
+var opAmount = 1;
+
 function boxT(){
   
   //frameRate(10);
@@ -40,20 +47,40 @@ function boxT(){
   angle += 0.005;
   degree += 0.005;
   
+ // if (mousePressed){degree +=0.3;}
+  
   //pop();
 
   
-  
+  op = 0;
   if (mouseX >= width/2-10 && mouseX <= width/2+10 && mouseY >= height/2 -30 && mouseY <= height/2 +30){
     degree = 0;
-    //for (size = 30; size < 100; size+=1){
-     size=80;
-     bg = 50;
-     // }
     
-  } else {
+     size=80;
+     bg = 10;
+     
+     if (mouseIsPressed){
+        bg= 30;
+        noStroke();
+        fill(255, 255, 255, 100);
+        rectMode(CENTER);
+        rect(mouseX-width/2, mouseY-height/2, 450,300);
+        LETTERS();
+        //if (millis() <10000){
+          for (var n= 0; n<10; n++){
+          angle +=0.005; 
+          //degree +=0.01;
+          }
+          
+          
+        //} 
+      } 
+     
+     }else {
     size = 30;
     bg= 0;
-  }
+    
+  } 
   
 }
+
