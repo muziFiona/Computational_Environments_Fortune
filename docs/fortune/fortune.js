897d061597ee053eb7558cfde371362999893ff5
Fiona Lei <56001183+muziFiona@users.noreply.github.com>
2020-06-08 02:08:10 +0100

    Update fortune.js

--- docs/fortune/fortune.js
+++ docs/fortune/fortune.js
@@ -3,12 +3,14 @@ let degree = 0;
 
 var x = [];
 var z = [];
-
+var button;
 
 let bg = 0;
+let myFont;
 //let size;
-
-
+function preload(){
+  myFont = loadFont('assests/Montserrat-Medium.ttf');
+}
 function setup() {
  createCanvas(windowWidth, windowHeight, WEBGL);
  //frameRate(30);
@@ -16,25 +18,67 @@ function setup() {
     x[i] = 600 * cos(14.5*radians(i));
     z[i] =  600 * sin(14.5*radians(i));
   }
+  
+
+  //button = createButton('welcome');
+ // background(bg);
+ //button.mousePressed(boxT);
+  rectCol=0;
+
+  runes[0]="FEHU \nGain \nHappiness";
+  runes[1]="URUZ \nYou need \nStrong Will";
+  runes[2]="THURISAZ \nResistance \nDefense \nTurning point";
+  runes[3]="ANSUZ \nSingals \nTrue Vision \nSelf-Knowledge";
+  runes[4]="RAIDHO \nJourney \nProcess \nThe Right Way";
+  runes[5]="KENAZ \nCreativity \nAwakening \nVision \nIllumination";
+  runes[6]="GEBO \nGift \nSharing \nBlessings \nPersonal Relationship";
+  runes[7]="WUNJO \nHarmony \nJoy of Life \nFellowship";
+  runes[8]="HAGALAZ \nStone \nWild Power \nDifficultity \nStroms";
+  runes[9]="NaUTHIZ \nConstraint \nNeed \nConflict \nDelays";
+  runes[10]="ISA \nConcentration \nNon-action \nFrozen in Time";
+  runes[11]="JeRA \nHarvest \nGain \nFruitful Season \nPeace and Happiness";
+  runes[12]="EIHWAZ \nPatience \nInitiation \nStrength \nEndurance";
+  runes[13]="PERTHRO \nVessel \nHidden Information \nSecrets";
+  runes[14]="ALGIZ \nProtection \nAwakening \nDefense \nConnection with Gods";
+  runes[15]="SOWILO \nSun \nGood Guidance \nHope \nGoals \nAchieved";
+  runes[16]="TEIWAZ \nLeadership \nWarrior \nSelf-Sacrifice \nVictory";
+  runes[17]="BERKANA \nGrowth \nNew Beginnings \nRenewal \nSpring";
+  runes[18]="ENWAZ \nHorse \nBalance \nHarmony \nTrust";
+  runes[19]="MANNAZ \nMankind \nAwareness \nKnowledge";
+  runes[20]="LAGUZ \nWater \nFlow \nEmotions \nEnergy";
+  runes[21]="INGWAZ \nFertility \nInternal Growth \nNew Life";
+  runes[22]="DAGAZ \nDawn \nBreakthrough \nThe Power to \nChange the Self";
+  runes[23]="OTHALA \nProperty \nPossessions \nAid in \nPhysical and Spiritual \nJourney";
+  runes[24]="Blank \nConsequences \nThe Unknown";
+
 }
 
 
 function draw() {
   //ambientLight(255);
+  //button.mousePressed(boxT);
   background(bg);
+  orbitControl();
+  
   //mousePt();
   //ambientMaterial(0,0,255);
   dotPt();
   push();
   //rotateY(angle);
+  
   boxT();
+  //runeStone.mousePressed(bg = 0);
   pop();
-  
   //RUNE();
-  
+
 }
 
-function mouseDragged(){
-  let camX = map(mouseX, 0, width, -400, 400);
-  camera(camX,0,(height/2)/tan(PI/6),0,0,0,0,1,0);
-}
+//function mouseDragged(){
+//  let camX = map(mouseX, 0, width, -400, 400);
+//  camera(camX,0,(height/2)/tan(PI/6),0,0,0,0,1,0);
+  
+//  fill (255);
+//  textSize(32);
+//  text("runes[index]", width/2,height/2);
+  
+//}
